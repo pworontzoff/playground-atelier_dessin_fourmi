5d7845617ad9a5c12bff4a9d19350d9a633b4122
pworontzoff <pierre.worontzoff@hepl.be>
2020-03-06 15:25:44 +0100

    Update paper.c

--- Fourmi/paper.c
+++ Fourmi/paper.c
@@ -111,16 +111,12 @@ void _create_table(struct spaper work) {
                         sprintf(buffer,"\n%.3f% {background-color: rgba(0,0,0,0)}",(pCur->numStep/(float)work.nbSteps)*100);
                         fputs(buffer,work.fp);
 		    } else {
-			// précédent transparent et actuel colorié
+			// précent transparent et actuel colorié
                         sprintf(buffer,"\n%.3f% {background-color: rgba(0,0,0,0)}",(pPrec->numStep/(float)work.nbSteps)*100);
                         fputs(buffer,work.fp);
                         sprintf(buffer,"\n%.3f% {background-color: rgba(0,0,0,0)}",((pCur->numStep/(float)work.nbSteps)*100)-0.001);
                         fputs(buffer,work.fp);
-                        sprintf(buffer,"\n%.3f% {background-color: rgb(255,0,0)}",(pCur->numStep/(float)work.nbSteps)*100);
-                        fputs(buffer,work.fp);
-                        sprintf(buffer,"\n%.3f% {background-color: rgb(255,0,0)}",((pCur->numStep+1)/(float)work.nbSteps)*100-0.001);
-                        fputs(buffer,work.fp);
-                        sprintf(buffer,"\n%.3f% {background-color: rgb(%d,%d,%d)}",((pCur->numStep+1)/(float)work.nbSteps)*100, pCur->current_color.red, pCur->current_color.green, pCur->current_color.blue);
+                        sprintf(buffer,"\n%.3f% {background-color: rgb(%d,%d,%d)}",(pCur->numStep/(float)work.nbSteps)*100, pCur->current_color.red, pCur->current_color.green, pCur->current_color.blue);
                         fputs(buffer,work.fp);
 		    }
 		} else {
@@ -131,11 +127,7 @@ void _create_table(struct spaper work) {
                         fputs(buffer,work.fp);
                         sprintf(buffer,"\n%.3f% {background-color: rgb(%d,%d,%d)}",((pCur->numStep/(float)work.nbSteps)*100)-0.001, pPrec->current_color.red, pPrec->current_color.green, pPrec->current_color.blue);
                         fputs(buffer,work.fp);
-                        sprintf(buffer,"\n%.3f% {background-color: rgb(255,0,0)}",(pCur->numStep/(float)work.nbSteps)*100);
-                        fputs(buffer,work.fp);
-                        sprintf(buffer,"\n%.3f% {background-color: rgb(255,0,0)}",(((pCur->numStep+1)/(float)work.nbSteps)*100)-0.001);
-                        fputs(buffer,work.fp);
-                        sprintf(buffer,"\n%.3f% {background-color: rgba(0,0,0,0)}",((pCur->numStep+1)/(float)work.nbSteps)*100);
+                        sprintf(buffer,"\n%.3f% {background-color: rgba(0,0,0,0)}",(pCur->numStep/(float)work.nbSteps)*100);
                         fputs(buffer,work.fp);
 		    } else {
 			// précent et actuel coloriés
