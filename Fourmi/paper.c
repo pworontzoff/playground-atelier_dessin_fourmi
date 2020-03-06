c8733dac7925325bf70d9c0ffa839c325371e223
pworontzoff <pierre.worontzoff@hepl.be>
2020-03-06 12:05:18 +0100

    Update paper.c

--- Fourmi/paper.c
+++ Fourmi/paper.c
@@ -116,7 +116,7 @@ void _create_table(struct spaper work) {
                         fputs(buffer,work.fp);
                         sprintf(buffer,"\n%.3f% {background-color: rgba(0,0,0,0)}",((pCur->numStep/(float)work.nbSteps)*100)-0.001);
                         fputs(buffer,work.fp);
-                        sprintf(buffer,"\n%.3f\% {background-color: rgb(%d,%d,%d)}",(pCur->numStep/(float)work.nbSteps)*100, pCur->current_color.red, pCur->current_color.green, pCur->current_color.blue);
+                        sprintf(buffer,"\n%.3f\% {background-color: rgb(127,127,127)}",(pCur->numStep/(float)work.nbSteps)*100, pCur->current_color.red, pCur->current_color.green, pCur->current_color.blue);
                         fputs(buffer,work.fp);
 		    }
 		} else {
