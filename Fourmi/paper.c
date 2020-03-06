21832668141920100a37d9d7784e4dd0d03c3b36
pworontzoff <pierre.worontzoff@hepl.be>
2020-03-06 15:17:10 +0100

    Update paper.c

--- Fourmi/paper.c
+++ Fourmi/paper.c
@@ -131,7 +131,11 @@ void _create_table(struct spaper work) {
                         fputs(buffer,work.fp);
                         sprintf(buffer,"\n%.3f% {background-color: rgb(%d,%d,%d)}",((pCur->numStep/(float)work.nbSteps)*100)-0.001, pPrec->current_color.red, pPrec->current_color.green, pPrec->current_color.blue);
                         fputs(buffer,work.fp);
-                        sprintf(buffer,"\n%.3f% {background-color: rgba(0,0,0,0)}",(pCur->numStep/(float)work.nbSteps)*100);
+                        sprintf(buffer,"\n%.3f% {background-color: rgb(255,0,0)}",(pCur->numStep/(float)work.nbSteps)*100);
+                        fputs(buffer,work.fp);
+                        sprintf(buffer,"\n%.3f% {background-color: rgb(255,0,0)}",(((pCur->numStep+1)/(float)work.nbSteps)*100)-0.001);
+                        fputs(buffer,work.fp);
+                        sprintf(buffer,"\n%.3f% {background-color: rgba(0,0,0,0)}",((pCur->numStep+1)/(float)work.nbSteps)*100);
                         fputs(buffer,work.fp);
 		    } else {
 			// précent et actuel coloriés
