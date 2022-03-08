f91ebf8f85935bf4592885097aba6788be3ac6c6
pworontzoff <pierre.worontzoff@hepl.be>
2022-03-08 14:12:55 +0100

    Add files via upload

---
+++ Paper_coeur_anim/main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "paper.h"
+
+int main()
+{
+    init_paper(13,13,25,0.5,0);
+
+    move_to(3,2);
+    
+    colorize();
+    
+    move_up();
+    
+    repeat(2) {
+        move_right();
+        colorize();
+    } loop;
+    
+    repeat(2) {
+        move_down();
+        move_right();
+        colorize();
+    } loop;
+    
+    repeat(2) {
+        move_up();
+        move_right();
+        colorize();
+    } loop;
+
+    move_right();
+    colorize();
+
+    repeat(2) {
+        move_down();
+        move_right();
+        colorize();
+    } loop;
+        
+    repeat(2) {
+        move_down();
+        colorize();
+    } loop;
+
+    repeat(5) {
+        move_down();
+        move_left();
+        colorize();
+    } loop;
+
+    repeat(5) {
+        move_up();
+        move_left();
+        colorize();
+    } loop;
+
+    repeat(2) {
+        move_up();
+        colorize();
+    } loop;
+
+    display_paper();
+    return 0;
+}
